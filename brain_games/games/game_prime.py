43f69d4694982b052b835aec22a68af05e49a3cc
Irina Shichkova <nhtnmzcktdf@gmail.com>
2024-10-26 03:29:28 +0300

    Refactoring game prime

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -3,21 +3,22 @@ from brain_games.constants import PRIME_TASK
 from brain_games.general_logic import start_game
 
 
-def get_random_num_and_check_prime():
-    num = randint(1, 101)
-    prime = 'yes'
-
+def check_prime(num):
     if num < 2:
-        prime = 'no'
-        return (num, prime)
+        return False
 
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            prime = 'no'
-            return (num, prime)
+            return False
 
+    return True
+
+
+def get_random_num_and_its_prime():
+    num = randint(1, 101)
+    prime = 'yes' if check_prime(num) else 'no'
     return (num, prime)
 
 
 def new_game():
-    start_game(PRIME_TASK, get_random_num_and_check_prime)
+    start_game(PRIME_TASK, get_random_num_and_its_prime)
