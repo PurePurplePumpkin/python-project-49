90d985ecf92e91798da754cf33f2808324f01220
Irina Shichkova <nhtnmzcktdf@gmail.com>
2024-10-26 03:33:15 +0300

    Correct typo

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -3,11 +3,11 @@ from brain_games.constants import CALC_TASK, MATH_OPERATORS
 from brain_games.general_logic import start_game
 
 
-def get_math_expression_and_chek_result():
+def get_math_expression_and_check_result():
     expression = f'{randint(1, 35)} {choice(MATH_OPERATORS)} {randint(1, 35)}'
     result = str(eval(expression))
     return (expression, result)
 
 
 def new_game():
-    start_game(CALC_TASK, get_math_expression_and_chek_result)
+    start_game(CALC_TASK, get_math_expression_and_check_result)
